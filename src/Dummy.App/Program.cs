1c14502d75cb7ec414345c0d1f61b4392b2390d2
Andrew <andrew.gubskiy@outlook.com>
2021-08-19 17:35:55 +0300

    Implement dummy job

---
+++ src/Dummy.App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Dummy.App
+{
+    class Program
+    {
+        private const string WorkingTime = "WORKING_TIME";
+        
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Start");
+            
+            var periodEnvironmentVariable = Environment.GetEnvironmentVariable(WorkingTime);
+
+            if (periodEnvironmentVariable == null)
+            {
+                throw new ArgumentException($"{WorkingTime} environment variable not defined");
+            }
+
+            if (int.TryParse(periodEnvironmentVariable, out var period))
+            {
+                var counter = 0;
+                
+                while (counter <= period)
+                {
+                    counter++;
+                    Thread.Sleep(1000);
+                    
+                    Console.WriteLine($"{DateTime.Now:g} Counter: {counter} Period: {period}");
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"{WorkingTime} environment variable is not valid integer value");
+            }
+            
+            Console.WriteLine("Finish");
+        }
+    }
+}
